bfa17426311c1482a7f8d5c4b1c05e8bb74955b2
dagarciagil <diegoalexandergarciagil@gmail.com>
2021-09-17 14:43:19 -0500

    Taller 3, avance

--- Taller3/src/main/java/co/edu/ucundinamarca/tallern/Main.java
+++ Taller3/src/main/java/co/edu/ucundinamarca/tallern/Main.java
@@ -1,6 +1,7 @@
 
 package co.edu.ucundinamarca.tallern;
 
+import java.util.Scanner;
 import org.apache.log4j.Logger;
 
 /**
@@ -12,8 +13,25 @@ public class Main {
     static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        log.debug("Hello World : this is a debug message");
-        log.info("Hello World : this is an info message");
+     Scanner sc = new Scanner(System.in);
+     int fac;
+    log.debug("Digite un numero: ");
+     fac = sc.nextInt();
+     Main objeto1 = new Main();
+     objeto1.factorial(fac);
+    log.info("El factorial del numero es: "+objeto1.factorial(fac));
+    
     }
 
+    int factorial(int num){ 
+     int resultado = 0;
+     
+     for(int i =num; i<1; i--){
+     resultado=num*i;
+     
+     }
+     
+    return resultado;
+    }
+    
 }

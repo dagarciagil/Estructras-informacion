85d1923b6d5524fc6294b846c79901542f4ac6fb
dagarciagil <diegoalexandergarciagil@gmail.com>
2021-09-24 14:56:50 -0500

    Tests

--- Taller3/src/test/java/co/edu/ucundinamarca/tallern/apartado2Test.java
+++ Taller3/src/test/java/co/edu/ucundinamarca/tallern/apartado2Test.java
@@ -2,26 +2,17 @@
 package co.edu.ucundinamarca.tallern;
 
 
-
-import co.edu.ucundinamarca.taller3.Main;
+import co.edu.ucundinamarca.taller3.apartado2;
 import static junit.framework.TestCase.assertTrue;
 import org.junit.Test;
 
-/**
- *
- * @author hortizr
- */
-public class MainTest {
-
-    @Test
+public class apartado2Test {
+     @Test
     public void testFactorial() {
     
-    Main fac = new Main();
+    apartado2 fac = new apartado2();
     int res = fac.factorial(5);
     assertTrue(res == 120);
    
     }
-
-   
-
 }
